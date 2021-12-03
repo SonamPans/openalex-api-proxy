1e49463b611fc4a82d3c07e01f13cfb0c70bef9c
Richard Orr <richard@ourresearch.org>
2021-12-03 14:04:52 -0500

    add rate limiting

--- app.py
+++ app.py
@@ -1,11 +1,60 @@
 import logging
+import os
 import sys
 import warnings
+from urllib.parse import urlparse
 
+import limits.errors
+import limits.util
+import redis
 from flask import Flask
 from flask_compress import Compress
+from limits.storage.redis import RedisStorage
 
 app = Flask(__name__)
+app.config['RATELIMIT_HEADERS_ENABLED'] = os.getenv('RATELIMIT_HEADERS_ENABLED')
+
+redis_url = os.getenv('REDIS_URL')
+app.config['RATELIMIT_STORAGE_URL'] = redis_url
+
+
+def redis_init(self, uri: str, **options):
+    """
+    :param uri: uri of the form `redis://[:password]@host:port`,
+     `redis://[:password]@host:port/db`,
+     `rediss://[:password]@host:port`, `redis+unix:///path/to/sock` etc.
+     This uri is passed directly to :func:`redis.from_url` except for the
+     case of `redis+unix` where it is replaced with `unix`.
+    :param options: all remaining keyword arguments are passed
+     directly to the constructor of :class:`redis.Redis`
+    :raise ConfigurationError: when the redis library is not available
+    """
+    redis_dependency = limits.util.get_dependency("redis")
+    if not redis_dependency:
+        raise limits.errors.ConfigurationError(
+            "redis prerequisite not available"
+        )  # pragma: no cover
+    uri = uri.replace("redis+unix", "unix")
+
+    redis_options = options.copy()
+    parsed_redis_url = urlparse(uri)
+
+    redis_options.update({
+        'host': parsed_redis_url.hostname,
+        'port': parsed_redis_url.port,
+        'username': parsed_redis_url.username,
+        'password': parsed_redis_url.password,
+        'ssl': True,
+        'ssl_cert_reqs': None
+    })
+
+    self.storage = redis.Redis(**redis_options)
+    self.initialize_storage(uri)
+    super(RedisStorage, self).__init__()
+
+
+RedisStorage.__init__ = redis_init
+
 
 logging.basicConfig(
     stream=sys.stdout,
--- views.py
+++ views.py
@@ -1,21 +1,56 @@
+import json
 import os
+from functools import wraps
 
-from flask import jsonify
+from flask import jsonify, make_response, abort
 from flask import request
+from flask_limiter import Limiter
 
 from app import app
 
 
-@app.route("/<path:request_path>", methods=["GET"])
-def handle_request(request_path):
+def abort_json(status_code, msg):
+    body_dict = {
+        "HTTP_status_code": status_code,
+        "message": msg,
+        "error": True
+    }
+    resp_string = json.dumps(body_dict, sort_keys=True, indent=4)
+    resp = make_response(resp_string, status_code)
+    resp.mimetype = "application/json"
+    abort(resp)
+
+
+def api_key_required(route):
+    @wraps(route)
+    def _api_key_required(*args, **kwargs):
+        if not request_api_key():
+            abort_json(422, "OpenAlex API key required")
+        return route(*args, **kwargs)
+    return _api_key_required
+
+
+def request_api_key():
+    return request.args.get('api_key') or request.headers.get('OpenAlex-API-Key')
+
+
+limiter = Limiter(app, key_func=request_api_key)
+
+
+@app.route('/<path:request_path>', methods=['GET', 'POST'])
+@api_key_required
+@limiter.limit('100000/day;20/second')
+def forward_request(request_path):
     return jsonify({
         'method': request.method,
         'path': request_path,
         'args': request.args,
+        'data': str(request.get_data()),
         'headers': dict(request.headers),
+        'api_key': request_api_key()
     })
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
